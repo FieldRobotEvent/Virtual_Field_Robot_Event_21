c55b9ff4c31134d02ec1bcc48efc820d8475f414
Gwen <petraea@nurdspace.nl>
2021-05-18 18:07:00 +0200

    Fix default.

--- cleanup_docker.sh
+++ cleanup_docker.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
 if [ -z "$1" ] ; then
-  $1="tidy"
-  echo "Defaulting to $1"
+  TASK="tidy"
+  echo "Defaulting to $TASK"
+else
+TASK=$1
 fi
 
-if [ "$1" == "stop" ] ; then
+if [ "$TASK" == "stop" ] ; then
   echo "Stopping all running containers."
   docker stop $(docker ps -q)
 fi
 
-if [ "$1" == "tidy" ] ; then
+if [ "$TASK" == "tidy" ] ; then
   echo "Removing all unlinked docker images."
   docker rmi $(docker images -q)
 fi
 
-if [ "$1" == "rmall" ] ; then
+if [ "$TASK" == "rmall" ] ; then
   echo "Stopping and removing all containers."
   echo "This may make you lose progress. ONLY do this if you are SURE you need to!"
   read -sp "Press return to continue."
@@ -23,7 +25,7 @@ if [ "$1" == "rmall" ] ; then
   docker rm $(docker ps -aq)
 fi
 
-if [ "$1" == "nuke" ] ; then
+if [ "$TASK" == "nuke" ] ; then
   echo "This may make you lose progress. ONLY do this if you are SURE you need to!"
   read -sp "Press return to continue."
   docker stop $(docker ps -q)
