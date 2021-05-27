0f2e8033a620da13988dc528f80607f7c09a7ddb
Gwen Dawes <gwen.dawes@wur.nl>
2021-05-27 18:36:21 +0200

    Cleanup and filter for existing things.

--- competition_environment/setup_for_compo_1.sh
+++ competition_environment/setup_for_compo_1.sh
@@ -4,29 +4,42 @@
 
 #/etc/docker/daemon.json still uses the default runtime as 'nvidia'. We probably need to test that one out...
 
-docker network create fre_default
+MYPATH=$(readlink -f $(dirname $0))
+
+NETNAME=fre_default
+if ! docker network ls --format "{{.Name}}" | grep "$NETNAME" ; then
+  docker network create "$NETNAME"
+fi
+
+A_NAME=fre_a_container_1
+if ! docker ps --format "{{.Names}}" | grep "$A_NAME" ; then
 docker run -d \
-  --name fre_a_container_1 \
+  --name "$A_NAME" \
   -h acontainer \
-  -v "/home/fre/catkin_ws/src/Virtual_Field_Robot_Event/competition_environment/task_1/worlds:/catkin/src/Virtual_Field_Robot_Event/virtual_maize_field/worlds" \
-  -v "/home/fre/catkin_ws/src/Virtual_Field_Robot_Event/competition_environment/task_1/Media:/catkin/src/Virtual_Field_Robot_Event/virtual_maize_field/Media" \
-  -v "/home/fre/catkin_ws/src/Virtual_Field_Robot_Event/competition_environment/task_1/map:/catkin/src/Virtual_Field_Robot_Event/virtual_maize_field/map" \
-  -v "/home/fre/catkin_ws/src/Virtual_Field_Robot_Event/competition_environment/task_1/launch:/catkin/src/Virtual_Field_Robot_Event/virtual_maize_field/launch" \
+  -v "${MYPATH}/task_1/worlds:/catkin/src/Virtual_Field_Robot_Event/virtual_maize_field/worlds" \
+  -v "${MYPATH}/task_1/Media:/catkin/src/Virtual_Field_Robot_Event/virtual_maize_field/Media" \
+  -v "${MYPATH}/task_1/map:/catkin/src/Virtual_Field_Robot_Event/virtual_maize_field/map" \
+  -v "${MYPATH}/task_1/launch:/catkin/src/Virtual_Field_Robot_Event/virtual_maize_field/launch" \
   -v "/tmp/.X11-unix:/tmp/.X11-unix:rw" \
   -v "/tmp/.docker.xauth:/tmp/.docker.xauth:rw" \
   -e "DISPLAY=:1" \
   -e "NVIDIA_DRIVER_CAPABILITIES=all" \
   --runtime nvidia \
   --gpus all \
-  --network fre_default \
+  --network "$NETNAME" \
   fieldrobotevent/a_container_2021
+fi
 
-#Check if b_task_1 exists then:
-#cd b_container && docker build -t b_task_1 . && cd
+B_IMAGENAME=b_task_1
+if ! docker images --format "{{.Repository}}" | grep "$B_IMAGENAME" ; then
+  cd b_container && docker build -t "$B_IMAGENAME" . && cd
+fi
 
+B_NAME=fre_b_container_1
+if ! docker ps --format "{{.Names}}" | grep "$B_NAME" ; then
 docker run -d \
-  --name fre_b_container_1 \
+  --name "$B_NAME" \
   -h bcontainer \
-  --network fre_default \
-  b_task_1
-
+  --network "$NETNAME" \
+  "$B_IMAGENAME"
+fi
