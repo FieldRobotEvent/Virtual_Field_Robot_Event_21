6b468073201ccb4c95b1dbbfa587e348273de412
Essen, Rick van <rick.vanessen@wur.nl>
2021-05-31 01:18:03 -0700

    Fix script

--- competition_environment/dump_my_container.sh
+++ competition_environment/dump_my_container.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 if [ -z $1 ] ; then
-  $1=1
-  echo "Defaulting to task $1"
+  TASK=1
+  echo "Defaulting to task $TASK"
+else
+  TASK=$1
 fi
-echo "Dumping for task $1"
+echo "Dumping for task $TASK"
 
 IMAGES=$(docker ps -a | tail -n +2 | sed 's/  */ /g')
-if echo $IMAGES | grep -v b_task_$1 ; then
-    echo "You don't have a b_task_$1 image present. Try starting the competition environment for task $1 first."
+if echo $IMAGES | grep -v b_task_$TASK ; then
+    echo "You don't have a b_task_$TASK image present. Try starting the competition environment for task $TASK first."
     exit 1
 fi
 
 
-echo "This script will dump the committed b_task_$1 image to a file. Be aware that you have to commit changes for this to work!"
+echo "This script will dump the committed b_task_$TASK image to a file. Be aware that you have to commit changes for this to work!"
 sleep 2
 
-echo "Dumping b_task_$1 to b_task_${1}.tgz now."
-docker image save b_task_$1 | gzip -c - > b_task_${1}.tgz
+echo "Dumping b_task_$TASK to b_task_${TASK}.tgz now."
+docker image save b_task_$TASK | gzip -c - > b_task_${TASK}.tgz
 echo "Please upload this file for competition purposes if your code is not public."
